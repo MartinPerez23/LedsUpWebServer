db1302d6ceb461d120b281d0350c60cca0fdb3be
MartinPerez23 <martin.perez@davinci.edu.ar>
2025-06-08 02:07:47 -0300

    Revert "Merge branch 'develop'"
    This reverts commit 776a7ab1e266883bd0c784937e279c4806cb1bb5, reversing
    changes made to 7b47fb4bc35370e5eddc3c60c43766f84ef9df83.

--- ledsup/consumers.py
+++ ledsup/consumers.py
@@ -1,48 +1,27 @@
 import json
 
-from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.db import close_old_connections
-from django.utils import timezone
-from oauth2_provider.models import AccessToken
+from django.conf import settings
 
 
 class RoomConsumer(AsyncWebsocketConsumer):
-    access_token = None
-    user = None
-    group_name = None
-
     async def connect(self):
-        # Extraer headers y token
+        # Obtener token de headers
         headers = dict((k.decode(), v.decode()) for k, v in self.scope["headers"])
-        raw_token = headers.get("authorization", "").replace("Bearer ", "")
+        token = headers.get("authorization", "").replace("Token ", "")
 
-        # Validar el token usando el modelo de AccessToken
-        try:
-            close_old_connections()
-            token_obj = await sync_to_async(AccessToken.objects.select_related("user").get)(token=raw_token)
-
-            # Verificar expiración del token
-            if token_obj.expires < timezone.now():
-                await self.close(code=4003)
-                return
-
-            self.access_token = raw_token
-            self.user = token_obj.user
-            self.group_name = f"token_{self.access_token}"
-
-        except AccessToken.DoesNotExist:
+        # Validar token
+        if token != settings.WS_SECRET_TOKEN:
             await self.close(code=4003)
             return
 
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.channel_layer.group_add("pc_apps", self.channel_name)
         await self.accept()
-        print(f"WebSocket conectado con token válido. Usuario: {self.user}, grupo: {self.group_name}")
+        print("WebSocket conectado y agregado a grupo pc_apps")
 
     async def disconnect(self, close_code):
-        if self.group_name:
-            await self.channel_layer.group_discard(self.group_name, self.channel_name)
-            print(f"Desconectado del grupo: {self.group_name}")
+        print("Desconectado")
+        await self.channel_layer.group_discard("pc_apps", self.channel_name)
 
     async def enviarAlServer(self, event):
         await self.send(text_data=json.dumps({
